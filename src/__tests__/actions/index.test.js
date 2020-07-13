a9f1e8587b4c4511d6eb63ee054a508ffff023b2
Jozy Kinnaman <jozypants@gmail.com>
2020-07-13 13:30:26 -0700

    add test for SEE_FORM. Expect fail but passed because Jozy is impatient.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,10 +1,15 @@
-import * as actions from "./../actions";
+import * as actions from "./../../actions";
 
 describe("flash card actions", () => {
   it ("seeLandingPage should create a LANDING_PAGE action", () => {
     expect(actions.seeLandingPage()).toEqual({
       type: 'LANDING_PAGE'
-    })
-  })
+    });
+  });
+  test("seeForm should create a SEE_FORM action", () => {
+    expect(actions.seeForm()).toEqual({
+      type: "SEE_FORM"
+    });
+  });
 })
 
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,7 @@
 export const seeLandingPage = () => ({
   type: 'LANDING_PAGE'
 });
+
+export const seeForm = () => ({
+  type: 'SEE_FORM'
+});
